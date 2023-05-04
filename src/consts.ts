75d9278ed8cc124fd1bd8fc3570eda10dfe7499e
Pedro <pedrosantos@bxblue.com.br>
2023-05-04 17:58:32 +0000

    updating inital page

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Dapedu\'s blog';
+export const SITE_DESCRIPTION = 'In this blog I\'ll store all things of my interest, so mostly will be technology';
