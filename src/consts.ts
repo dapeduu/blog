a93a7e6fd87b3bb2b0e10c9e5797a1a91b153c7b
Pedro Santos <pedrosantosdevelop@gmail.com>
2024-05-26 16:50:54 -0300

    adds the initial hero

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,10 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'DAPEDU';
+export const SITE_DESCRIPTION = 'Bem vindo ao meu blog! Meu nome é Pedro Santos (dapedu é o apelido 🙂), sou um desenvolvedor de software, especializado em desenvolvimento web e com um pézinho em inteligência artificial. Eu posso tornar o seu projeto dos sonhos em realidade!';
+export const SOCIAL_LINKS = {
+    "linkedin": "https://www.linkedin.com/in/dapedu/",
+    "github": "https://github.com/dapeduu",
+    "email": "mailto:pedrosantosdevelop@gmail.com"
+}
